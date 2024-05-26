2052313e186cbb4d067c9ffc261d099bda906f0a
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2024-05-26 23:42:23 +0300

    пеквая функция

--- repo_STL.cpp
+++ repo_STL.cpp
@@ -3,8 +3,14 @@
 using namespace std;
 
 int main() {
-
-
-
+	// empty пуст, size размер ,push нажать ,pop хлопать ,top сверху;
+	stack<int> numbersStack;
+	 
+	if (numbersStack.empty()) {
+		cout << "\tSteck is empty";
+	}
+	else {
+		cout << "\tSteck is not empty";
+	}
 	system("pause>0");
 }
