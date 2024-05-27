a69928f82ea84d8a6531509e6d47ef85df40efb8
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2024-05-27 23:28:05 +0300

    init mep

--- repo_STL.cpp
+++ repo_STL.cpp
@@ -1,28 +1,6 @@
 ﻿#include <iostream>
-#include <queue>
 using namespace std;
-// funkciya kotoraya pechataet polnuyu ochered
-void printQueue(queue<int> queue) {
-	while (!queue.empty()){
-		cout << queue.front() << " ";
-		queue.pop();
-	}
-	cout << endl;
-}
-
-
 int main() {
-	cout << "\tochered\n";
-	queue<int>myQueue;
-	myQueue.push(1);//1funkciya dobavleniya v konec ocheredi
-	myQueue.push(2);
-	myQueue.push(3);
-	cout << "Size is " << myQueue.size() << endl;
-	cout << "First element is " << myQueue.front() << endl;
-	cout << "Last element is" << myQueue.back() << endl;
-
-	cout << "My queue :";
-	printQueue(myQueue);
-
+	
 	system("pause>0");
 }
