01745bcb60fc929008f6519e972c1b86e22698ff
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2024-05-27 21:55:50 +0300

    top

--- repo_STL.cpp
+++ repo_STL.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include <stack>
 using namespace std;
+ 
+void printStackElements (stack<int> stack) {
+	while (!stack .empty()){
+		cout << stack.top() << endl;
+		stack.pop();
+	}
+}
 
 int main() {
 	// empty пуст, size размер ,push -> добавляет, pop -> удаляет элемент изверхней части стэка 
@@ -9,9 +16,8 @@ int main() {
 	numbersStack.push(1);
 	numbersStack.push(2);
 	numbersStack.push(3);
-	numbersStack.pop();
-
-
+	
+    /*
 	if (numbersStack.empty()) {
 		cout << "\tSteck is empty\n";
 	}
@@ -20,5 +26,11 @@ int main() {
 	}
 
 	cout << "\tSteck size is " << numbersStack.size() << endl;
+	*/
+	
+	printStackElements(numbersStack);
+
+
+
 	system("pause>0");
 }
