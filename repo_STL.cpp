dac87a19693037a53df5c8b28b8118f5e2672090
DIM <4ebotarov_dim@inbox.ru>
2024-05-27 22:37:54 +0300

    raspechatka vsey ocheredi

--- repo_STL.cpp
+++ repo_STL.cpp
@@ -1,6 +1,15 @@
 ﻿#include <iostream>
 #include <queue>
 using namespace std;
+// funkciya kotoraya pechataet polnuyu ochered
+void printQueue(queue<int> queue) {
+	while (!queue.empty()){
+		cout << queue.front() << " ";
+		queue.pop();
+	}
+	cout << endl;
+}
+
 
 int main() {
 	cout << "\tochered\n";
@@ -11,5 +20,9 @@ int main() {
 	cout << "Size is " << myQueue.size() << endl;
 	cout << "First element is " << myQueue.front() << endl;
 	cout << "Last element is" << myQueue.back() << endl;
+
+	cout << "My queue :";
+	printQueue(myQueue);
+
 	system("pause>0");
 }
