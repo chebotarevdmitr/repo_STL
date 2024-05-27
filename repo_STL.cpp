4e8f46924a5f1161e88e7310f3104c21b338402a
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2024-05-27 22:20:30 +0300

    queue funkcii

--- repo_STL.cpp
+++ repo_STL.cpp
@@ -1,8 +1,15 @@
 ﻿#include <iostream>
+#include <queue>
 using namespace std;
+
 int main() {
-
-
-
+	cout << "\tochered\n";
+	queue<int>myQueue;
+	myQueue.push(1);//1funkciya dobavleniya v konec ocheredi
+	myQueue.push(2);
+	myQueue.push(3);
+	cout << "Size is " << myQueue.size() << endl;
+	cout << "First element is " << myQueue.front() << endl;
+	cout << "Last element is" << myQueue.back() << endl;
 	system("pause>0");
 }
