27f53bcec3af6fe30b8c43f5c140b2f017cd54d7
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2024-05-31 21:07:47 +0300

    raspechatka map

--- repo_STL.cpp
+++ repo_STL.cpp
@@ -7,7 +7,12 @@ using namespace std;
 int main() {
 	map<string, string> myDictionary;
 	myDictionary.insert(pair<string, string>("apple", "der Apfel"));
+	myDictionary.insert(pair<string, string>("banana", "die Banane"));
+	myDictionary.insert(pair<string, string>("orange", "die Orange , die Apfelsine"));//apelsin
+	myDictionary.insert(pair<string, string>("strawberry", "die Erdbeere"));//klubnika
 
-
+	for (auto pair : myDictionary) {
+		cout << pair.first << " - " << pair.second << endl;
+	}
 	system("pause>0");
 }
