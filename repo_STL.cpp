2560eed9f229cbab3ccd5f562c3da1de9db886a8
DIM <4ebotarov_dim@inbox.ru>
2024-05-31 20:48:55 +0300

    first element map

--- repo_STL.cpp
+++ repo_STL.cpp
@@ -1,6 +1,13 @@
 ﻿#include <iostream>
+#include<map>
+#include<string>
+
 using namespace std;
+
 int main() {
-	
+	map<string, string> myDictionary;
+	myDictionary.insert(pair<string, string>("apple", "der Apfel"));
+
+
 	system("pause>0");
 }
